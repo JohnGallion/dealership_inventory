7f69c6e094cd09b6ae2db1fb2b1ae6347b231d12
zjjkee <zjjkee@gmail.com>
2023-03-26 02:16:58 +1300

    update

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+const authRoute = require("./routes/auth-route");
+const profileRoute = require("./routes/profile-route");
+require("./config/passport");
+const passport = require("passport");
+const session = require("express-session");
+const flash = require("connect-flash");
+
+
+// middleware
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_ms");
+  res.locals.error = req.flash("error");
+  // console.log("res.locals:",res.locals)
+  next();
+});
+app.use("/auth", authRoute);
+app.use("/profile", profileRoute);
+
+app.get("/", (req, res) => {
+  res.render("index", { user: req.user });
+});
+
+app.listen(7070, () => {
+  console.log("Server running on port 7070.");
+});
---
+++ routes/profile-route.js
@@ -0,0 +1,41 @@
+const router = require("express").Router();
+// const Post = require("../models/post-model");
+const { Sequelize, Model, DataTypes } = require("sequelize");
+const {post} = require("../models");
+
+const authCheck = (req, res, next) => {
+  console.log(req.originalUrl);
+  req.session.returnTo = req.originalUrl;
+  if (!req.isAuthenticated()) {
+    res.redirect("/auth/login");
+  } else {
+    next();
+  }
+};
+
+
+router.get("/", authCheck, async (req, res) => {
+  console.log(req.user)
+  let postFound = await post.findAll({where:{ author: req.user.dataValues.id }});
+  console.log(postFound)
+  res.render("profile", { user: req.user, posts: postFound });
+});
+
+router.get("/post", authCheck, (req, res) => {
+  res.render("post", { user: req.user });
+});
+
+router.post("/post", authCheck, async (req, res) => {
+  let { make,type,year,price } = req.body;
+  // await post.create({title:title,content:content,author: req.user.id})
+  await post.create({make:make,type:type,year:year,price:price,author: req.user.id})
+  try {
+    // await newPost.save();
+    res.status(200).redirect("/profile");
+  } catch (err) {
+    req.flash("error_msg", "Both title and content are required.");
+    res.redirect("/profile/post");
+  }
+});
+
+module.exports = router;
